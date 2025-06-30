a7213c765bc86824f12227ca89f2ecc52b5baa2d
lgguan <lgguan@live.cn>
2025-06-30 18:41:48 +0800

    适配Vercel

--- api/index.php
+++ api/index.php
@@ -1,8 +1,11 @@
 <?php
 // api/index.php
 
+// 获取项目根目录
+$rootPath = dirname(__DIR__);
+
 // 设置应用目录
-define('APP_PATH', __DIR__ . '/../application/');
+define('APP_PATH', $rootPath . '/application/');
 
 // 加载框架引导文件
-require __DIR__ . '/../thinkphp/start.php';
+require $rootPath . '/thinkphp/start.php';
