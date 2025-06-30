eb9fa7c7eb60da63ffb8acdb8df8f7e12a10ecb5
lgguan <lgguan@live.cn>
2025-06-30 19:00:01 +0800

    适配Vercel

--- api/index.php
+++ api/index.php
@@ -1,23 +1,6 @@
 <?php
-// 显示所有错误
-ini_set('display_errors', 1);
-error_reporting(E_ALL);
-
-// 输出环境信息
-echo "<pre>";
-echo "Current directory: " . getcwd() . "\n";
-echo "Root directory listing:\n";
-var_dump(scandir('/var/task/user'));
-echo "\nAPI directory listing:\n";
-var_dump(scandir('/var/task/user/api'));
-echo "</pre>";
-exit;
-
-// 获取项目根目录
-$rootPath = dirname(__DIR__);
-
-// 设置应用目录
-define('APP_PATH', $rootPath . '/application/');
-
-// 加载框架引导文件
-require $rootPath . '/thinkphp/start.php';
+/**
+ * Here is the serverless function entry
+ * for deployment with Vercel.
+ */
+require __DIR__.'/../public/index.php';
